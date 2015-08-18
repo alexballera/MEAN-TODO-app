bbe5ed61dcf4da6c2a2d30149cfddf392b07bf86
alexballera <alexballera@gmail.com>
2015-08-18 17:53:29 -0430

    Estructurando en modulos, servicios y controladores

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -1,40 +1,35 @@
-//Creación del Módulo
-var meanTodo = angular.module('meanTodo', []);
+angular.module('todoController', []);
 
-//Creación del Controlador 
-function mainController($scope, $http) {
-  $scope.formData = {};
+  .controller('mainController', function($scope, $http) {
+    $scope-formData = {};
 
-  // Cuando se cargue la página, pide del API todos los TODOs
-  $http.get('/api/todos/')
-    .success(function(data) {
-      $scope.todos = data;
-      console.log(data);
-    })
-    .error(function(data) {
-      console.log('Error: ' + data);
-    });
-  // Cuando se añade un nuevo TODO, manda el texto a la API
-  $scope.createTodo = function() {
-    $http.post('/api/todos/', $scope.formData)
+    $http.get('/api/todos')
       .success(function(data) {
-        $scope.formData = {};
         $scope.todos = data;
-        console.log(data);
       })
       .error(function(data) {
         console.log('Error: ' + data);
       });
-  };
-  //Borra un TODO luego de chequearlo como finalizado
-  $scope.deleteTodo = function(id) {
-    $http.delete('/api/todos/' + id)
-      .success(function(data) {
-        $scope.todos = data;
-        console.log(data);
-      })
-      .error(function(data) {
-        console.log('Error: ' + data);
-      });
-  };
-}
+
+    $scope.createTodo = function() {
+      $http.post('/api/todos', $scope.formData)
+        .success(function(data) {
+          $scope.formData = {};
+          $scope.todos = data;
+        })
+        .error(function(data) {
+          console.log('Error: ' + data);
+        });
+    };
+
+    $scope.deleteTodo = function(id) {
+      $http.delete('/api/todos/' + id)
+        .success(function(data) {
+          $scope.todos = data;
+        })
+        .error(function(data) {
+          console.log('Error: ' + data);
+        });
+    };
+
+  });
